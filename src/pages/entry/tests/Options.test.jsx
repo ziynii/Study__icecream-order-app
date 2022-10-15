696369bf598ca799062e89721fa70fa2e85c1637
Jiyeon <ziyeon.ivi@gmail.com>
2022-10-16 04:15:42 +0900

    Code and test scoop options images from server

---
+++ src/pages/entry/tests/Options.test.jsx
@@ -0,0 +1,15 @@
+import { render, screen } from '@testing-library/react';
+import Options from '../Options';
+
+test('display image for each scope option from server', async () => {
+  render(<Options optionType="scoops" />);
+
+  // find images
+  const scoopImages = await screen.findAllByRole('img', { name: /scoop$/i });
+
+  expect(scoopImages).toHaveLength(2);
+
+  // confirm alt text of images
+  const altText = scoopImages.map((image) => image.alt);
+  expect(altText).toEqual(['Chocolate scoop', 'Vanilla scoop']);
+});
---
+++ src/pages/summary/OurSummary.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const OurSummary = () => {
+	return (
+		<div>
+			
+		</div>
+	);
+};
+
+export default OurSummary;
